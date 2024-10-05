8e021717e039659e6e08625a4b3fbb295918e216
Manan <104508861+mananchhikara@users.noreply.github.com>
2024-10-05 18:09:50 +0530

    Add files via upload

---
+++ PetPals-CodingChallenge/PetPals/Model/AdoptionEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetPals.Model
+{
+    public class AdoptionEvent
+    {
+        public int EventID { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; }
+
+        public List<Participant> Participant { get; set; } = new List<Participant>();
+        public DateTime Date { get;  set; }
+
+        public void RegisterParticipant(IAdoptable participant)
+        {
+            Participant.Add((Participant)participant);
+        }
+
+        public void HostEvent()
+        {
+            Console.WriteLine("Adoption Event is being hosted.");
+        }
+    }
+}
